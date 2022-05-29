1814242f6363457acca74f9ee405fc2fe0220cc0
Sergio Montiel <sergiomontielcastello@gmail.com>
2022-05-29 17:40:21 -0500

    Commit Producto pt1, controlador a medias, vistas, rutas, y test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #Ruta POST, metodo create, para guardar un nuevo producto en la BD
+  post "products", to: "products#create"
+  #Ruta GET, metodo new, as: es el metodo helper para no poner la ruta en nuestro codigo
+  get "/products/new", to: "products#new", as: :new_product
+  #Ruta GET, controlador products, metodo index, vista index, products_path()
+  get "/products", to: "products#index"
+  #Ruta GET con id, metodo show, as: :product para product_path()
+  get "/products/:id", to: "products#show", as: :product
 end
